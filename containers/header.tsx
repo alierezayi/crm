d94d7922de9dc97c81e8568ff90a8bb93a2013aa
Ali Rezaei <im1.bitcode@gmail.com>
2024-07-14 11:57:27 +0330

    implement root layout - basic header, footer, sidebar

---
+++ containers/header.tsx
@@ -0,0 +1,5 @@
+export default function Header() {
+  return (
+    <div className="h-16 bg-gray-500">header</div>
+  )
+}
---
+++ containers/sidebar.tsx
@@ -0,0 +1,7 @@
+export default function Sidebar() {
+  return (
+    <div className="w-[250px] bg-gray-900 dark:bg-gray-300 text-white dark:text-black">
+      sidebar
+    </div>
+  );
+}
