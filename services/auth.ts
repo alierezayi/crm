a27b81c7d0d8edb4532e380047d68ee5de885944
Ali Rezaei <im1.bitcode@gmail.com>
2024-07-14 14:52:23 +0330

    install axios - configuration apis - 2

--- services/auth.ts
+++ services/auth.ts
@@ -1,9 +1,35 @@
 import api from "@/configs/api";
-import { LoginFormType } from "@/lib/types";
 
-export const sendLoginData = async (data: LoginFormType) => {
+export const loginAPI = async (data: any) => {
   try {
     const response = await api.get("api/Admin/login");
+    return { response };
+  } catch (error) {
+    return { error };
+  }
+};
+
+export const login2FaAPI = async (data: any) => {
+  try {
+    const response = await api.get("api/Admin/login-2fa");
+    return { response };
+  } catch (error) {
+    return { error };
+  }
+};
+
+export const registerAPI = async (data: any) => {
+  try {
+    const response = await api.post("api/Admin/register", data);
+    return { response };
+  } catch (error) {
+    return { error };
+  }
+};
+
+export const logoutAPI = async (data: any) => {
+  try {
+    const response = await api.get("api/Admin/logout");
     return { response };
   } catch (error) {
     return { error };
