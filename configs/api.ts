1daaa5a70017ee372710ccee4a373cc3e77a066f
Ali Rezaei <im1.bitcode@gmail.com>
2024-07-20 09:48:42 +0330

    add domain in backend url, api request

--- configs/api.ts
+++ configs/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const api = axios.create({
-  baseURL: "http://95.217.228.239:5000/",
+  baseURL: "http://manager-prop2.unfxco.com:5000/",
   headers: {
     "Content-Type": "application/json",
   },
